a13d4688566d1a6a34968527d2ff9b461c599cd5
aluno <aluno@DSN1251017051>
2022-05-09 09:08:01 -0300

    hj b

---
+++ Polimorfismo/src/Aplicacao.java
@@ -0,0 +1,27 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Animal animal = new Animal();
+		animal.Falar();
+		
+		animal = new Gato();
+		animal.Falar();
+		
+		Gato gato = new Gato();
+		Cachorro cachorro = new Cachorro();
+		falar(gato);
+		falar(cachorro);
+
+	}
+	public static void falar(Animal a) {
+		a.Falar();
+		
+		if(a instanceof Cachorro) {
+			Cachorro c = (Cachorro) a;
+			c.Morder();
+		}
+	}
+
+}
---
+++ Polimorfismo/src/Cachorro.java
@@ -0,0 +1,14 @@
+
+public class Cachorro extends Animal{
+
+	@Override
+	public void Falar() {
+		System.out.println("AU-AU");
+	}
+	
+	public void Morder() {
+		System.out.println("NHAC!!");
+	}
+	
+	
+}
