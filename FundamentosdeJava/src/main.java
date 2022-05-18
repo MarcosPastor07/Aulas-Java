b07724da084424d10920b02a96dadfd9ef5cab68
aluno <aluno@DSN1251017051>
2022-05-18 10:34:03 -0300

    Connit lista

--- FundamentosdeJava/src/main.java
+++ FundamentosdeJava/src/main.java
@@ -87,46 +87,72 @@ public class main {
 //		System.out.println("Taboada do 2");
 //		for(int x=0; x<11; x++) {
 //			System.out.println("2 x " + x + " = " + x * 2);
+//		
+//		int x1 = 3;
+//		int y1 = 5;
+//		int z1 = 7;
+//		
+//		System.out.println(x1);
+//		System.out.println(y1);
+//		System.out.println(z1);
+//
+//		double x [] = {3, 5, 3, 5};
+//		
+//		for (int aux = 0; aux < x.length; aux++) {
+//			System.out.println("Valor do índice " + aux + " = " + x[aux]);
+//		}
+//		String nome[] = { "Rafael", "Lara", "Gislene"};
+//		
+//		for (int aux1 = 0; aux1 < nome.length; aux1++) {
+//			System.out.println(nome[aux1]);
+//			if (nome[aux1] == "Gislene") {
+//				System.out.println("O índice de Gislene é = " + aux1);
+//			}
+//		}
+//		
+//		int [] arrayInt = new int[5];
+//		String [] ArrayString = new String[4];
+//		double[] ArrayDouble = new double[3];
+//		
+//		arrayInt[3] = 4;
+//		
+//		int[] numero = new int [6];
+//		Scanner teclado = new Scanner(System.in);
+//		
+//		for (int aux2 = 0; aux2 < numero.length; aux2++) {
+//			System.out.println("Digite um número qualquer");
+//			numero[aux2] = teclado.nextInt();
+//		}
+//		for (int aux2 = 0; aux2 < numero.length; aux2++) {
+//			System.out.println("Índice " +aux2+ " = " + numero[aux2]);
+//		}
+//		interar é a forma de ver cada posiçao/valor da matriz
+//		int[] arrayQualquer = new int[3];
+//		Object[] arrayObject = new Object[5];
+//		
+//		arrayObject[0] = 12;
+//		arrayObject[1] = "Marcos";
+//		arrayObject[2] = 13.8;
+//		arrayObject[3] = true;
+//		arrayObject[4] = arrayQualquer;
+//		
+//		for (int aux2 = 0; aux2 < arrayObject.length; aux2++) {
+//			System.out.println("Índice " +aux2+ " = " + arrayObject[aux2]);
+//		}
+//		System.out.println();
 		
-		int x1 = 3;
-		int y1 = 5;
-		int z1 = 7;
+		int [][] arrayBidirecional = new int[4][3];
 		
-		System.out.println(x1);
-		System.out.println(y1);
-		System.out.println(z1);
-
-		double x [] = {3, 5, 3, 5};
+		arrayBidirecional[0][0]=8;
 		
-		for (int aux = 0; aux < x.length; aux++) {
-			System.out.println("Valor do índice " + aux + " = " + x[aux]);
+		arrayBidirecional[2][1]=7;
+		
+		for (int linha = 0; linha < arrayBidirecional.length; linha++) {
+		for (int coluna = 0; coluna < arrayBidirecional[0].length; coluna++) {
+			System.out.print("Índice [" +linha+ "][" +coluna +"] = " + arrayBidirecional[linha][coluna]+ " ");
 		}
-		String nome[] = { "Rafael", "Lara", "Gislene"};
-		
-		for (int aux1 = 0; aux1 < nome.length; aux1++) {
-			System.out.println(nome[aux1]);
-			if (nome[aux1] == "Gislene") {
-				System.out.println("O índice de Gislene é = " + aux1);
-			}
+		System.out.println();
 		}
-		
-		int [] arrayInt = new int[5];
-		String [] ArrayString = new String[4];
-		double[] ArrayDouble = new double[3];
-		
-		arrayInt[3] = 4;
-		
-		int[] numero = new int [6];
-		Scanner teclado = new Scanner(System.in);
-		
-		for (int aux2 = 0; aux2 < numero.length; aux2++) {
-			System.out.println("Digite um número qualquer");
-			numero[aux2] = teclado.nextInt();
-		}
-		for (int aux2 = 0; aux2 < numero.length; aux2++) {
-			System.out.println("Índice " +aux2+ " = " + numero[aux2]);
-		}
-  		}
-
+	}
 	}
 
---
+++ List/src/main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		List lista = new ArrayList();
+	
+//		lista.add("abc");
+//		lista.add(12);
+//		lista.add(true);
+//		for(int x=0; x< lista.size(); x++) {
+//			System.out.println(lista.get(x));
+//	}
+//		for(Object valor : lista) {
+//			System.out.println(valor);
+//		}
+//		System.out.println();
+//		lista.remove(0);
+//		for(Object valor : lista) {
+//			System.out.println(valor);
+//		}
+//		System.out.println();
+//		lista.add(0, "def");
+//		for(Object valor : lista) {
+//			System.out.println(valor);
+//		}
+//		System.out.println();
+//		lista.set(0, "ghi");
+//		for(Object valor : lista) {
+//			System.out.println(valor);
+//		}
+//		
+//		System.out.println();
+//		lista.clear();
+//		if(lista.isEmpty()) {
+//			System.out.println("Sua lista está vazia");
+//		}	
+		
+		Conta conta1 = new Conta("Agua", 60.00);
+		Conta conta2 = new Conta("Luz", 120.00);
+		
+		List<Conta> listaDeContas = new ArrayList<Conta>();
+		
+		listaDeContas.add(conta1);
+		listaDeContas.add(conta2);
+		
+		for(Conta conta : listaDeContas) {
+			System.out.println(conta);
+		}
+		
+
+	}
+
+}
