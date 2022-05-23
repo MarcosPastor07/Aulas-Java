4cdba66d1e0d7fcb185161d103f04a79b602896a
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-23 12:01:33 -0300

    Update AreaPoligonos.java

--- Somapolígonos/src/AreaPoligonos.java
+++ Somapolígonos/src/AreaPoligonos.java
@@ -2,20 +2,19 @@
 public class AreaPoligonos {
 
 
-int somar(int x, int y) {
-		
+int somar(int x, int y) { //Criando o método somar do tipo int
 		//int z = x * y;
 		//return z;
-		return x * y;
+		return x * y; //definindo que esse método irá retornar o valor de x*y
 	}
 	
-	double somar(double x, double y) {
+	double somar(double x, double y) { //Criando o método somar do tipo double
 		
-		return x + y / 2;
+		return x + y / 2; //Definindo que esse método irá retornar o valor de x+y/2
 		}
 	
-	 int somar2(int x, int y) {
+	 int somar2(int x, int y) { //Criando o método somar2 do tipo int
 		
-		return x * y;
+		return x * y; //definindo que esse método irá retornar o valor de x*y
 		}
 }
