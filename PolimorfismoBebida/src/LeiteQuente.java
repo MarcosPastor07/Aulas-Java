4cc889c7f2414f44cf650148f7e41ae52e48c6aa
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-23 13:18:10 -0300

    Update LeiteQuente.java

--- PolimorfismoBebida/src/LeiteQuente.java
+++ PolimorfismoBebida/src/LeiteQuente.java
@@ -1,12 +1,12 @@
 
-public class LeiteQuente extends Bebida {
+public class LeiteQuente extends Bebida { // A classe LeiteQuente é uma subclasse que estende de Bebida
 
-	public LeiteQuente() {
+	public LeiteQuente() { // criando o construtor
 		super("Leite Quente", true);
 	}
 
 	@Override
-	public void preparar() {
+	public void preparar() { //
 		super.preparar();
 		System.out.println("Colocando o leite no copo");
 	}
