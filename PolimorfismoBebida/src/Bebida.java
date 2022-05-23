9c483f0bf2991d61e17eab8513af53a61512c43c
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-23 13:05:16 -0300

    Update Bebida.java

--- PolimorfismoBebida/src/Bebida.java
+++ PolimorfismoBebida/src/Bebida.java
@@ -1,19 +1,21 @@
 
 public class Bebida {
-		
+	
+	// Criando atributos	
 	private String nome;
 	private boolean aquecer;
 	
-	public Bebida(String nome, boolean aquecer) {
+	public Bebida(String nome, boolean aquecer) { //Criando o Construtor de Bebida
 		super();
 		this.nome = nome;
 		this.aquecer = aquecer;
 	}
 	
-	public void preparar() {
+	public void preparar() { // Criando o método preparar e utilizando "void", pois esse método não irá retornar nenhum valor para o usuário		
 		System.out.println("Pegando o copo");
 	}
 
+	// Utilizando o método get e set. O set serve para alterar o valor de um atributo e o get para pegar/acessar o valor de um atributo
 	public String getNome() {
 		return nome;
 	}
