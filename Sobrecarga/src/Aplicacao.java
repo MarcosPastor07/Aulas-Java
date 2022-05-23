043f26fe42d077ce77188d811aed17acb996dec0
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-23 12:42:31 -0300

    Update Aplicacao.java

--- Sobrecarga/src/Aplicacao.java
+++ Sobrecarga/src/Aplicacao.java
@@ -1,30 +1,30 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importando o Scanner
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);	
+		Scanner scanner = new Scanner(System.in); // Instanciando o Scanner	
 		
 		
-		Matematica matematica = new Matematica();
+		Matematica matematica = new Matematica(); // Instanciando o Objeto Matematica
 		
-		System.out.println("Somar dois números");
-		System.out.println("Digite o primeiro número");
-		int x = scanner.nextInt();
+		System.out.println("Somar dois nÃºmeros"); // Utilizando o syso para mostrar a mensagem no console
+		System.out.println("Digite o primeiro nÃºmero");
+		int x = scanner.nextInt(); // Utilizando o Scanner para permitir que o usuÃ¡rio insira um valor que irÃ¡ ficar armazenado na variÃ¡vel x
 		
-		System.out.println("Digite o segundo número");
+		System.out.println("Digite o segundo nÃºmero");
 		int y = scanner.nextInt();
 			
 		
 		int soma = matematica.somar(x, y);
-			System.out.println("O resultado é: " + soma);
+			System.out.println("O resultado Ã©: " + soma); // Utilizando o syso para mostrar uma mensagem no console, e usando o + para concatenar a mensagem com o resultado
 			
 			soma = matematica.somar(x, y, 5);
-			System.out.println("O resultado é: " + soma);
+			System.out.println("O resultado Ã©: " + soma);
 			
 		double soma2 = matematica.somar(x, y);
-			System.out.println("O resultado é: " + soma2);
+			System.out.println("O resultado Ã©: " + soma2);
 			
 	}
 
