90a7fb8040d59b24cf21c0ea4f3716397370f5c4
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-23 12:49:43 -0300

    Update Matematica.java

--- Sobrecarga/src/Matematica.java
+++ Sobrecarga/src/Matematica.java
@@ -1,11 +1,11 @@
 
 public class Matematica {
 		
-	int somar(int x, int y) {
+	int somar(int x, int y) { // Criando o método somar do tipo int
 		
 		//int z = x + y;
 		//return z;
-		return x + y;
+		return x + y; // Mostrando que esse método irá retornar o valor de x+y
 	}
 	
 	int somar(int x, int y, int z) {
