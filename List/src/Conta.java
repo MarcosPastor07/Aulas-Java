544835ac05453b8b5462b1c955f1005509dfd787
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-24 09:05:25 -0300

    Update Conta.java

--- List/src/Conta.java
+++ List/src/Conta.java
@@ -1,17 +1,18 @@
 
 public class Conta {
 	
+	//Criando Atributos
 	String nome;
 	double valor;
 	
-	public Conta(String nome, double valor) {
+	public Conta(String nome, double valor) { //Criando o método Conta
 		this.nome = nome;
 		this.valor = valor;
 	}
 
 	@Override
-	public String toString() {
-		return "Conta [nome = " + nome + ", valor = " + valor + "]";
+	public String toString() { //Fazendo Override para chamar um método já existente de uma outra classe
+		return "Conta [nome = " + nome + ", valor = " + valor + "]"; //Retorno que esse método irá trazer após ser executado
 	}
 	
 	
