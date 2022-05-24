3328ad8d1c5e839e5f8a167dc95cfdf809cabbe2
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-24 08:10:51 -0300

    Update main.java

--- PolimorfismoBebida/src/main.java
+++ PolimorfismoBebida/src/main.java
@@ -3,15 +3,15 @@ public class main {
 
 	public static void main(String[] args) {
 
-		Preparador preparador = new Preparador();
+		Preparador preparador = new Preparador(); //Instanciando um objeto 
 		
 		LeiteQuente leitequente = new LeiteQuente();
-		preparador.prepararBebida(leitequente);
+		preparador.prepararBebida(leitequente); //Chamando o método preparar bebida da subclasse leitequente
 		
-		System.out.println();
+		System.out.println(); //syso vazio para pular linha
 		
-		Refrigerante refrigerante = new Refrigerante();
-		preparador.prepararBebida(refrigerante);
+		Refrigerante refrigerante = new Refrigerante(); //Instanciando um novo objeto
+		preparador.prepararBebida(refrigerante); //Chamando o método preparar bebida da subclasse refrigerante
 		
 		
 	}
