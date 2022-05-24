d1157a5612acf9717a538dcee4ed3f53acc88937
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-24 09:48:55 -0300

    Update Item.java

--- ClassesAbstratas/src/Item.java
+++ ClassesAbstratas/src/Item.java
@@ -1,24 +1,24 @@
 
 public abstract class Item {
 		
-	private int posX; // Criando uma vari·vel privada do tipo inteiro
-	private int posY; // Criando uma vari·vel privada do tipo inteiro
+	private int posX; // Criando uma vari√°vel privada do tipo inteiro chamada "PosX"
+	private int posY; // Criando uma vari√°vel privada do tipo inteiro chamada "PosY"
 	
-	public abstract void pegar();  // Sempre que estende de um Item abstrato, temos que implementar os mÈtodos da classe que est· sendo estendida
+	public abstract void pegar();  // Sempre que estende de um Item abstrato, temos que implementar os m√©todos da classe que est√° sendo estendida
 		
-	public int getPosX() { // utilizando o mÈtodo getters and setters
+	public int getPosX() { // utilizando o m√©todo getters and setters
 		return posX;
 	}
 
-	public void setPosX(int posX) { // utilizando o mÈtodo getters and setters
+	public void setPosX(int posX) { // utilizando o m√©todo getters and setters
 		this.posX = posX;
 	}
 
-	public int getPosY() { // utilizando o mÈtodo getters and setters
+	public int getPosY() { // utilizando o m√©todo getters and setters
 		return posY;
 	}
 
-	public void setPosY(int posY) { // utilizando o mÈtodo getters and setters
+	public void setPosY(int posY) { // utilizando o m√©todo getters and setters
 		this.posY = posY;
 	}
 	
