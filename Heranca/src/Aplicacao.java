c71f9609ad3fa0d533ad0177c1f0e1323c857b05
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-24 09:45:44 -0300

    Update Aplicacao.java

--- Heranca/src/Aplicacao.java
+++ Heranca/src/Aplicacao.java
@@ -3,16 +3,17 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Veiculo veiculo = new Veiculo();
+		Veiculo veiculo = new Veiculo(); //Instanciando um objeto do tipo Veiculo
 		
+		//Aplicando valores/nomes nos atributos: Marca, Modelo e Ano. O método Setter permite que façamos isso
 		veiculo.setMarca("Kawazaki");
 		veiculo.setModelo("Z 300");
 		veiculo.setAno(2019);
 		
-		veiculo.imprimir();
-		veiculo.buzinar();
+		veiculo.imprimir(); //Chamando o método imprimir
+		veiculo.buzinar(); //Chamando o método buzinar
 		
-		Carro carro = new Carro();
+		Carro carro = new Carro(); //Instanciando um objeto do tipo Carro
 		
 		carro.setMarca("Suzuki");
 		carro.setModelo("Grand Vittara");
