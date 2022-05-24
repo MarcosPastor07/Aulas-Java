15b48ef3ac0d4304933e159ea7ef17095fc4808c
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-24 08:55:47 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,13 +1,13 @@
 
-public class Cachorro extends Animal{
+public class Cachorro extends Animal{ //Classe Cachorro que estende de Animal
 
 	@Override
-	public void Falar() {
-		System.out.println("AU-AU");
+	public void Falar() { //Fazendo um Override para chamar o método Falar que está na classe Animal
+		System.out.println("AU-AU"); //Mensagem que deve ser exibida após esse método ser executado
 	}
 	
-	public void Morder() {
-		System.out.println("NHAC!!");
+	public void Morder() { //Criando o método Morder
+		System.out.println("NHAC!!"); //Mensagem que deve ser exibida após esse método ser executado
 	}
 	
 	
