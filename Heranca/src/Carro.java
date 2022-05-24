2c045915b80fd9cdf5acbfea850fd8632aa1b4b4
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-05-24 09:38:07 -0300

    Update Carro.java

--- Heranca/src/Carro.java
+++ Heranca/src/Carro.java
@@ -1,21 +1,21 @@
 
 public class Carro extends Veiculo{
 	
-	private boolean quatroPortas;
+	private boolean quatroPortas; //Criando um atributo que apenas o carro pode possuir
 	
 	
 	
 	@Override
-	public void imprimir() {
+	public void imprimir() { //Override para chamar um método já existente em outra classe
 		super.imprimir();
-		System.out.println("Tem quatro portas: " + quatroPortas);
+		System.out.println("Tem quatro portas: " + quatroPortas); //syso para mostrar uma mensagem
 	}
 
-	public boolean isQuatroPortas() {
-		return quatroPortas;
+	public boolean isQuatroPortas() { //Se for quatro portas mesmo, irá retornar o valor verdadeiro
+		return quatroPortas; //retorno
 	}
 
-	public void setQuatroPortas(boolean quatroPortas) {
+	public void setQuatroPortas(boolean quatroPortas) { //método setters
 		this.quatroPortas = quatroPortas;
 	}
 	
