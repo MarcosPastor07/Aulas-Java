d90748cfb1243c0ebbdd55ce7ee865598d53d36a
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-06-02 10:31:46 -0300

    Update Sabonete.java

--- LerTXT/src/Sabonete.java
+++ LerTXT/src/Sabonete.java
@@ -1,9 +1,10 @@
 public class Sabonete {
 	
+	//Criando atributos
 	String cheiro;
 	String cor;
 	
-	public String toString() { 
+	public String toString() { //Usando o metodo to string para trazer uma representação textual de uma instância de um objeto
 		return "Sabonete: Cheiro = " + cheiro  + ", Cor = " + cor + "\n"; 
 	}
 	
