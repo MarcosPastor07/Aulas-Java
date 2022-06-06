73a3a064d0b1da0e99d432fe646fb5c42fe6a05d
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-06-06 09:11:11 -0300

    Update ContaInvestimento.java

--- ExercicioContaBancaria/src/ContaInvestimento.java
+++ ExercicioContaBancaria/src/ContaInvestimento.java
@@ -1,6 +1,7 @@
 
 public class ContaInvestimento extends ContaBancaria {
 
+	//Criando o metodo de como o Saldo sera calculado dentro de uma conta Investimento
 	@Override
 	public void calcularSaldo() {
 		System.out.println(saldo + (saldo * 0.5));
