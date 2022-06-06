7a7795af7179e53c40e0eb29c46ec0b1abfd7006
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-06-06 09:09:46 -0300

    Update ContaCorrente.java

--- ExercicioContaBancaria/src/ContaCorrente.java
+++ ExercicioContaBancaria/src/ContaCorrente.java
@@ -1,7 +1,7 @@
 
 public class ContaCorrente extends ContaBancaria { 
 
-	//Criando o método de como o saldo sera calculado
+	//Criando o método de como o saldo sera calculado dentro de uma conta corrente
 	@Override
 	public void calcularSaldo() {
 		System.out.println(saldo - (saldo * 0.1));
