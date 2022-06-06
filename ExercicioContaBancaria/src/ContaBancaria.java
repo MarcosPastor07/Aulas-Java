d4aefbe14e954b3c7ec5d24a90770f17bb4b44dd
MarcosPastor07 <102963859+MarcosPastor07@users.noreply.github.com>
2022-06-06 09:07:44 -0300

    Update ContaBancaria.java

--- ExercicioContaBancaria/src/ContaBancaria.java
+++ ExercicioContaBancaria/src/ContaBancaria.java
@@ -1,23 +1,23 @@
 
 public abstract class ContaBancaria {
 
-	protected double saldo;
+	protected double saldo; //Criando um atributo protected para que somente as classes herdeiras tenham acesso a ele.
 
-	public void depositar(double valor) {
+	public void depositar(double valor) { //Criando metodo para depositar
 		 saldo += valor;
 		
 	}
 		
-	public void sacar(double valor) {
+	public void sacar(double valor) { //Criando metodo para sacar
 		 saldo -= valor;
 		
 	}
 	
-	public void transferir(double valor) {
+	public void transferir(double valor) { //Criando metodo para transferir
 		 saldo -= valor;
 	}
 	
-	public void calcularSaldo() {
+	public void calcularSaldo() { //Chamando o metodo de calcular o saldo
 		
 	}
 }
