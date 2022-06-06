e1c6c762c6e6fcf8742b2e7d831fd3c34c35f6a9
aluno <aluno@DSN1251017051>
2022-06-06 10:40:46 -0300

    Commit - MDA

---
+++ ModificadorDeAcesso/src/Cliente.java
@@ -0,0 +1,66 @@
+import java.util.Objects;
+
+public class Cliente {
+	
+	//Criando atributos privados
+	private String nome;
+	private int cpf;
+	private  int numConta;
+	
+	
+	public Cliente() {
+		
+	}
+	
+	//Utilizando metodos getters and setters para acessar e modificar valores
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	public int getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+	
+	//Utilizando o Override para subscrever um metodo ja existente 
+	@Override
+	public String toString() {
+		return "Cliente:\nNome= " + nome + ",\nCPF= " + cpf + ",\nNúmero da Conta= " + numConta ;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, nome, numConta);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return cpf == other.cpf && Objects.equals(nome, other.nome) && numConta == other.numConta;
+	}
+	
+	
+
+}
+
---
+++ ModificadorDeAcesso/src/ContaBancaria.java
@@ -0,0 +1,35 @@
+public class ContaBancaria {
+	 
+	//Criando atributos privadaos de vários tipos
+	private	int numConta;
+	private	boolean ativo;
+	private	double saldo;
+	
+	//Criando o método contabancaria que recebe os valores de ativo e saldo
+	public ContaBancaria() {
+		ativo = true;
+		saldo = 1.0;
+		
+	}
+	
+	public ContaBancaria(boolean ativo, double saldo) {
+		this.ativo = ativo;
+		this.saldo = saldo;
+	}
+
+	public void receber(double valor) {
+		saldo += valor;
+	}
+	public double saldo() {
+		return saldo;
+	}
+	public void dar (double valor) {
+		if(saldo>=valor) {
+			saldo -= valor;
+	}
+	}
+		
+	}
+
+
+
