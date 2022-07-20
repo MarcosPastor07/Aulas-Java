ec041ae4a058481df2c2e759faca999112f1c864
Aluno <Aluno@DSN1251210691>
2022-07-20 11:37:51 -0300

    Revisao

---
+++ Exercícios/src/Fundamento/ProgramaRevisao.java
@@ -0,0 +1,76 @@
+package Fundamento;
+
+public class ProgramaRevisao {
+
+	String Nome;
+	int Idade;
+	int Cpf;
+	double Salario;
+//	int sub;
+	
+	public static void Nome(String nextLine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void Idade(int nextInt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void Cpf(int nextInt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void Salario(double nextDouble) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		this.Nome = nome;
+	}
+
+	public int getIdade() {
+		return Idade;
+	}
+
+	public void setIdade(int idade) {
+		this.Idade = idade;
+	}
+
+	public int getCpf() {
+		return Cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.Cpf = cpf;
+	}
+
+	public double getSalario() {
+		return Salario;
+	}
+
+	public void setSalario(double salario) {
+		this.Salario = salario;
+	}
+
+//	public int getSub() {
+//		return sub;
+//	}
+//
+//	public void setSub(int sub) {
+//		this.sub = sub;
+//	}
+
+	
+	
+
+}
